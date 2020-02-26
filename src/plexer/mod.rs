c2fdfbe6897685a2048753a251ec4f7f3bb65195
Jonathan Whittle <jonathan.whittle@eltoro.com>
2020-02-25 20:58:59 -0500

    fix errors

--- src/plexer/mod.rs
+++ src/plexer/mod.rs
@@ -1,6 +1,8 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Method, Request, Response, StatusCode, Uri};
 
+type HandleFn = fn(Request<Body>) -> Result<Response<Body>, hyper::Error>;
+
 #[derive(Debug)]
 struct Plexer<'a> {
     routes: Vec<&'a Route>,
@@ -8,30 +10,37 @@ struct Plexer<'a> {
 
 impl Plexer<'_> {
     pub fn new() -> Plexer<'static> {
-        Plexer { routes: Vec::new() }
+        Plexer {
+            routes: Vec::<&Route>::with_capacity(1),
+        }
     }
     pub fn register(&mut self, r: &'static Route) {
         self.routes.push(r);
     }
-    pub fn dispatch(&self) {
-        let service =
-            make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(place_holder)) });
-    }
+    pub fn dispatch(&self) {}
 }
 
-async fn place_holder(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    Ok(Response::new(Body::from("Yup")))
+trait Handle {
+    fn new(path: String, hf: HandleFn) -> Self;
+    fn handle(&self, r: Request<Body>) -> Result<Response<Body>, hyper::Error>;
 }
 
 #[derive(Debug)]
-struct Route<F: Fn(Request<Body>) -> Result<Response<Body>, hyper::Error>> {
+struct Route {
     path: String,
     methods: Vec<Method>,
-    handler: F,
+    handler: HandleFn,
 }
 
-impl<F: Fn(Request<Body>) -> Result<Response<Body>, hyper::Error>> Route<F> {
-    pub fn handler(&self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-        (self.handler)(req)
+impl Handle for Route {
+    fn new(path: String, hf: HandleFn) -> Route {
+        Route {
+            path: path,
+            methods: vec![],
+            handler: hf,
+        }
+    }
+    fn handle(&self, r: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+        (self.handler)(r)
     }
 }
